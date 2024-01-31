688563e595cb8a9d7bc511b06bbd2b895806f4d4
postittom3@gmail.com <pathuri.sanketh@gmail.com>
2024-01-31 22:49:55 +0530

    added login and signup pages

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -10,18 +10,18 @@ export default function () {
       <Appbar />
       <div className="flex mt-10 justify-center items-center">
         <div className="w-1/2 flex-col justify-around ">
-          <h1>Fill the detials to Login</h1>
+          <h1>Fill the detials to Signup</h1>
           <br></br>
           <Input placeholder="enter username"></Input>
           <br></br>
           <Input placeholder="enter password"></Input>
           <div className="flex justify-center">
             <div>
-              <Button className="mt-3 ml-4 bg-green-700">LOGIN</Button>
+              <Button className="mt-3 ml-4 bg-green-700">Signup</Button>
               <p>
                 new here{" "}
-                <Link href="signup" className="hover:text-red-500">
-                  Signup
+                <Link href="login" className="hover:text-red-500">
+                  Login
                 </Link>
               </p>
             </div>
